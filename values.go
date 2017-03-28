5163b78b54f529996e97fc0d3bedc2411680c4a2
Kimduytuan <kdt@kdts-MacBook-Pro.local>
2017-03-28 18:45:40 +0700

    Values

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main()  {
+  fmt.Println("Chao mung cac ban" + " den voi khoa hoc lap trinh Golang")
+  fmt.Println("Gia tri cua 100 + 100 = ", 100+100)
+  fmt.Println("Gia tri cua 8.0/3.0 = ", 8.0/3.0)
+  fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+
+}
